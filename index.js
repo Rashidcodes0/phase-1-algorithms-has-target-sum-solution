6c2189ec58142f1329e5fac98a4c2bd0dcfa6dfd
Abdirashidnoor <abdirashid16112@gmail.com>
2023-04-02 14:06:16 +0300

    assingment completed

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  const map = {}; // create an empty hash map
+
+  for (let i = 0; i < array.length; i++) {
+    const complement = target - array[i];
+    if (map[complement] !== undefined) {
+      return true;
+    }
+    map[array[i]] = i; // insert the current element and its index to the hash map
+  }
+  return false; // if the loop ends  without getting complementary elements, return false
 }
+
 
 /* 
   Write the Big O time complexity of your function here
+  - This function iterates over the input array once, so the time complexity is O(n), where n is the length of the array.
 */
 
 /* 
   Add your pseudocode here
+ * Sort the input array.
+*While the left pointer is less than the right pointer, check if the sum of the values at the pointers equals the target.
+*If the sum is less than the target, increment the left pointer, otherwise decrement the right pointer.
+*If no pair is found that equals the target, return false.
 */
 
 /*
